14903ae3a0f950bd603de0d76fb55f85b0a3f2ef
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-10-04 00:22:05 +1000

    Add move and turn functionality

--- app/classes/Pacman.js
+++ app/classes/Pacman.js
@@ -1,37 +1,100 @@
 class Pacman {
   constructor() {
-    this.positionX = '';
-    this.positionY = '';
-    this.orientation = '';
+    this.positionX;
+    this.positionY;
+    this.orientation;
+    
+    this.dirObj = {
+      0: 'NORTH',
+      1: 'EAST',
+      2: 'SOUTH',
+      3: 'WEST'
+    };
   }
 
-  face(orientation) {
+  // Set initial orientation when placed on the board
+  turn(direction) {
     // Rotate pacman 90 degrees depending on input
-    if (orientation === 'LEFT') {
+    if (direction == 'LEFT') {
       // Move to the orientation left of current orientation
       // e.g. if current orientation == 'N' and input == 'LEFT', set orientation to 'W'
       // if current orientation == 'E' and input == 'RIGHT', set orientation to 'S'
       // etc.
+      let newOrientation;
+      
+      Object.keys(this.dirObj).forEach(key => {
+        if (this.dirObj[key] == this.orientation) {
+          // validate key:
+          // if key - 1 == -1, no index exists
+          key = key - 1 == -1 ? 0 : parseInt(key) - 1;
+          newOrientation = this.dirObj[key];
+        }
+      });
+
+      return this.orientation = newOrientation;
+
+    } else if (direction == 'RIGHT') {
+      let newOrientation;
+      
+      Object.keys(this.dirObj).forEach(key => {
+        
+        if (this.dirObj[key] == this.orientation) {
+          // validate key:
+          // if key + 1 == 4, no index exists in `dirObj`
+          key = key + 1 == 4 ? 0 : parseInt(key) + 1;
+
+          newOrientation = this.dirObj[key];
+        }
+      });
+
+      return this.orientation = newOrientation;
     }
-    
+  }
+
+  setPosition(x, y, f) {
+    this.positionX = x;
+    this.positionY = y;
+    this.orientation = f;
+  }
+
+  checkMove(x, y, gameboard) {
+    if (x < 0) {
+      return false;
+    } else if (y < 0) {
+      return false;
+    } else if (x > gameboard.length) {
+      return false;
+    } else if (y > gameboard.height) {
+      return false;
+    }
+
+      return true;
   }
   
-  move() {
-    if (this.orientation == 'N') {
-      // y + 1
+  // Move by one in the direction of `this.orientation`
+  move(gameboard) {
+    if (this.orientation == 'NORTH') {
+      this.positionY++;
     }
-    if (this.orientation == 'E') {
-      // x + 1
+    if (this.orientation == 'EAST') {
+      this.positionX++;
     }
-    if (this.orientation == 'S') {
-      // y -1
+    if (this.orientation == 'SOUTH') {
+      this.positionY--;
     }
-    if (this.orientation == 'W') {
-      // x - 1
+    if (this.orientation == 'WEST') {
+      this.positionX--;
     }
+
+    if ( this.checkMove(this.positionX, this.positionY, gameboard) ) {
+      return `${this.positionX},${this.positionY},${this.orientation}`;
+    }
+
+    return console.log("Please try again");
 
   }
 
+  // Print current position and orientation
   report() {
     return `${this.positionX},${this.positionY},${this.orientation}`;
   }
