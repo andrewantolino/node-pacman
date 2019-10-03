3dac9841cb23873d7f301b802eca079078e6c6aa
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-10-04 00:25:51 +1000

    Derive user input from ie's provided sample

--- app/board.js
+++ app/board.js
@@ -1,9 +1,61 @@
-const GameBoard = require('./classes/GameBoard').default;
+const GameBoard = require('./classes/GameBoard');
 const Pacman = require('./classes/Pacman');
 
 // entry point of the app
 // feed commands to the classes here and simulate user input
+const gameBoard = new GameBoard(4, 4);
 
-GameBoard.createGameBoard(4, 4);
+gameBoard.createGameBoard();
 
-module.exports = { createGameBoard, place };
+const pacman = new Pacman();
+
+// get values 1, 1 and 'E' from user input somehow?
+
+// a)
+// input:
+// PLACE 0,0,NORTH
+var input = [0, 0, "NORTH"];
+
+gameBoard.place(...input);
+pacman.setPosition(...input);
+
+pacman.move(gameBoard);
+
+console.log(pacman.report()); // 0,1,NORTH
+
+
+// b)
+// input: 
+// PLACE 0,0,NORTH
+// LEFT
+// REPORT
+var input2 = [0, 0, "NORTH"];
+
+gameBoard.place(...input2);
+pacman.setPosition(...input2);
+
+pacman.turn("LEFT");
+
+console.log(pacman.report()); // 0,0,WEST
+
+
+// c)
+// input:
+// PLACE 1,2,EAST
+// MOVE
+// MOVE
+// LEFT
+// MOVE
+// REPORT
+
+var input3 = [1, 2, "EAST"];
+
+gameBoard.place(...input3);
+pacman.setPosition(...input3);
+
+pacman.move(gameBoard);
+pacman.move(gameBoard);
+pacman.turn("LEFT");
+pacman.move(gameBoard);
+
+console.log(pacman.report()); // 3,3,NORTH
